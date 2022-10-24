2ed2de924e29b26173dfca1b47e9a16f85adc13f
Sydney166 <SydneyElliott.166@outlook.com>
2022-10-23 23:05:28 -0500

    Create grocery.ts

---
+++ grocery.ts
@@ -0,0 +1,11 @@
+
+class Grocery {
+  name: string;
+  quantity: number;
+  constructor(name: string, quantity: number) {
+    this.name = name;
+    this.quantity = quantity;
+  }
+}
+
+const grocery_List = ["4 bananas", "2 carrots", "1 milk"];
